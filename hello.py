428c54e1b19a93358e349b00a27db94a0bd1fda2
rubyinhell <rubyinhell@gmail.com>
2015-06-09 00:02:56 +0800

    add readme and 404

--- hello.py
+++ hello.py
@@ -19,7 +19,8 @@ def index():
         for i in records:
         	print _uniout.unescape(i[5], 'utf8')
         return render_template('index.html', records=records)
-    return 'You are not logged in  </br><a href="login" class="btn btn-primary" role="button">Login</a>'
+    return 'You are not logged in  </br><a href="login" class="btn btn-primary" role="button">Login</a> \
+            <a href="bendon" class="btn btn-primary" role="button">Bendon</a>'
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -38,6 +39,23 @@ def logout():
     # remove the username from the session if it's there
     session.pop('username', None)
     return redirect(url_for('index'))
+
+@app.route('/bendon', methods=['GET', 'POST'])
+def bendon():
+    # bendom
+    if request.method == 'POST':
+        session['username'] = request.form['username']
+        return 'You are : %s' %session['username']
+    return '''
+        <form action="" method="post">
+            <p><input type=text name=username>
+            <p><input type=submit value=Login>
+        </form>
+    '''
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'),404
 
 app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
 if __name__ == '__main__':
